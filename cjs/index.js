ecd684baffca51e3243cb28228cf596ab6cf55eb
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-08-30 16:49:14 +0200

    exported client too to simplify connect and query

--- cjs/index.js
+++ cjs/index.js
@@ -4,8 +4,8 @@ const re = new RegExp(uid, 'g');
 const filled = (tpl, i = 1) => tpl.join(uid).replace(re, () => '$' + i++);
 const rows = result => result.rows;
 const shift = result => rows(result).shift();
-
 module.exports = client => ({
+  client,
   all: (tpl, ...values) => client.query(filled(tpl), values).then(rows),
   get: (tpl, ...values) => client.query(filled(tpl), values).then(shift)
 });
