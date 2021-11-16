54b9342ecb88d4de7828b18af57182ce4406178f
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-11-16 11:29:19 +0100

    Updated dependencies

--- cjs/index.js
+++ cjs/index.js
@@ -1,9 +1,9 @@
 'use strict';
-const plain = (m => m.__esModule ? /* istanbul ignore next */ m.default : /* istanbul ignore next */ m)(require('plain-tag'));
+const plain = (m => /* c8 ignore start */ m.__esModule ? m.default : m /* c8 ignore stop */)(require('plain-tag'));
 const {asStatic, asParams} = require('static-params');
 
 const create = (pool, $) => (tpl, ...values) => {
-  if (tpl.some(invalid))
+  if (tpl.some(chunk => chunk.includes('$')))
     throw new Error('SQL injection hazard');
   const [sql, ...params] = asParams(tpl, ...values);
   return pool.query(sql.map(holes).join(''), params).then($);
@@ -11,16 +11,12 @@ const create = (pool, $) => (tpl, ...values) => {
 
 const holes = (value, i) => (0 < i ? ('$' + i) : '') + value;
 
-const invalid = chunk => chunk.includes('$');
-
-const raw = (tpl, ...values) => asStatic(plain(tpl, ...values));
-
 function PGTag(pool) {
   return {
     all: create(pool, $ => $.rows),
     get: create(pool, $ => $.rows.shift()),
     query: create(pool, $ => $),
-    raw,
+    raw: (tpl, ...values) => asStatic(plain(tpl, ...values)),
     pool
   };
 }
--- esm/index.js
+++ esm/index.js
@@ -2,7 +2,7 @@ import plain from 'plain-tag';
 import {asStatic, asParams} from 'static-params';
 
 const create = (pool, $) => (tpl, ...values) => {
-  if (tpl.some(invalid))
+  if (tpl.some(chunk => chunk.includes('$')))
     throw new Error('SQL injection hazard');
   const [sql, ...params] = asParams(tpl, ...values);
   return pool.query(sql.map(holes).join(''), params).then($);
@@ -10,16 +10,12 @@ const create = (pool, $) => (tpl, ...values) => {
 
 const holes = (value, i) => (0 < i ? ('$' + i) : '') + value;
 
-const invalid = chunk => chunk.includes('$');
-
-const raw = (tpl, ...values) => asStatic(plain(tpl, ...values));
-
 export default function PGTag(pool) {
   return {
     all: create(pool, $ => $.rows),
     get: create(pool, $ => $.rows.shift()),
     query: create(pool, $ => $),
-    raw,
+    raw: (tpl, ...values) => asStatic(plain(tpl, ...values)),
     pool
   };
 };
